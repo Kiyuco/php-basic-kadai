e6961375ba733d2e67df903c5330a9d5782edfc7
Kiyuco <yukingworld.g@gmail.com>
2025-02-22 11:41:45 +0900

    kadai_005再提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,13 +9,21 @@
 <body>
   <p>
     <?php
-    $numbers =[80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+    $score1 =80;
+    $score2 =60;
+    $score3 =55;
+    $score4 =40;
+    $score5 =100;
+    $score6 =25;
+    $score7 =80;
+    $score8 =95;
+    $score9 =30;
+    $score10 =60;
 
-    $sum = array_sum($numbers);
+    $sum = $score1 +$score2 +$score3 +$score4 +$score5 +$score6 +$score7 +$score8 +$score9 +$score10 ;
 
-    $count = count($numbers);
 
-    $average = $sum / $count;
+    $average = $sum / 10;
 
     echo "平均値：". $average;
 
